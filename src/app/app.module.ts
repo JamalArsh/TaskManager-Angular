d4d708c3df51787377c7dfdec8edbb445ac5128d
JamalArsh <92667028+JamalArsh@users.noreply.github.com>
2023-06-08 14:50:38 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditListComponent } from './pages/edit-list/edit-list.component';
+import { EditTaskComponent } from './pages/edit-task/edit-task.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditListComponent,
+    EditTaskComponent,
+    LoginComponent,
+    NewListComponent,
+    NewTaskComponent,
+    SignUpComponent,
+    TaskViewComponent
   ],
   imports: [
     BrowserModule,
